536de8b6f567239a5f72b98a4628a6ea9dc29c63
Nandha Kumar <79451582+nandhakumar18@users.noreply.github.com>
2022-01-11 18:42:18 +0530

    security updates

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 const auth = require('./middleware/authentication')
+// security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
 // connect db
 const connectDb = require('./db/connect')
 // routers
@@ -16,8 +21,19 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 const res = require('express/lib/response')
 
 app.use(express.static('./static', { extensions: ['html', 'htm'] }))
+
+app.set('trust proxy', 1)
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 200,
+  }),
+)
 app.use(express.json())
 // extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
